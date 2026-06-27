618e0f0c3e6a1c19f7b4f73666eaa7590a97b403
rajnitrrcse <rajbxr46@gmail.com>
2026-06-27 15:07:55 +0530

    Changing mail port

--- routes/user.js
+++ routes/user.js
@@ -12,11 +12,13 @@ const upload = multer({ storage })
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 function generateOtp() {
